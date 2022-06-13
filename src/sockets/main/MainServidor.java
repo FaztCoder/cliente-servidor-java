985b6856874ec64a99c1ac85ef6d4acb17ebe245
Anthony Solano Lopez <anthonysn16@icloud.com>
2022-06-12 20:15:06 -0500

    cliente servidor

---
+++ src/sockets/main/MainServidor.java
@@ -0,0 +1,14 @@
+package sockets.main;
+
+import java.io.IOException;
+import sockets.servidor.Servidor;
+
+public class MainServidor {
+  public static void main(String[] args) throws IOException {
+    Servidor serv = new Servidor(); // Se crea el servidor
+
+    System.out.println("Iniciando servidor\n");
+    serv.startServer(); // Se inicia el servidor
+
+  }
+}
